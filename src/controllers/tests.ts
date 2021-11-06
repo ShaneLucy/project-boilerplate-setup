763f4ddc0210649ee63520d20f124d76eadad604
Shane Lucy <shanelucy01@protonmail.com>
2021-11-06 13:49:01 +0000

    Added todos

---
+++ src/controllers/tests.ts
@@ -0,0 +1,10 @@
+/**
+ * TODO:
+ * Create logic to set up test suite
+ * Install jest
+ * Install ts-jest
+ * Create jest.config.js
+ * Create test command
+ * Generate code coverage report
+ * If project is frontend, install playwright
+ */
